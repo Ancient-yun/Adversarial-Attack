583a3110d4689db013c65f7075b1334a4687c58c
daeyun <eodbs117848@gmail.com>
2026-02-03 05:08:56 +0000

    데이터 경로 수정 및 이미지 변경

--- mmsegmentation/configs_attack/cityscapes/config_deeplabv3.py
+++ mmsegmentation/configs_attack/cityscapes/config_deeplabv3.py
@@ -2,7 +2,7 @@ config = {
     "attack_method": "Pixel",
     "task": "segmentation",
     "dataset": "cityscapes",
-    "data_dir": "datasets/cityscapes",         # Directory path where the dataset is located
+    "data_dir": "../datasets/cityscapes",         # Directory path where the dataset is located
     "model": "deeplabv3",
     "RGB": 3,                                       # Input dimension
     "attack_pixel": 0.01,                              # Attack dimension for the Remember process (recalculated later) previow 5
--- mmsegmentation/configs_attack/cityscapes/config_setr.py
+++ mmsegmentation/configs_attack/cityscapes/config_setr.py
@@ -4,7 +4,7 @@ config = {
     "attack_method": "Pixel",
     "task": "segmentation",
     "dataset": "cityscapes",
-    "data_dir": "datasets/cityscapes",         # Directory path where the dataset is located
+    "data_dir": "../datasets/cityscapes",         # Directory path where the dataset is located
     "model": "setr",
     "RGB": 3,                                       # Input dimension
     "attack_pixel": 0.01,                              # Attack dimension for the Remember process (recalculated later)
